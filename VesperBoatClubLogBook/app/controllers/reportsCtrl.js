fd09a002fbbc5d3e85f6509ffbd031a1ab4b821c
mjpiarulli <mjpiarulli@gmail.com>
2018-05-24 22:10:10 -0400

    added editing boat status logs to boat damage report

--- VesperBoatClubLogBook/app/controllers/reportsCtrl.js
+++ VesperBoatClubLogBook/app/controllers/reportsCtrl.js
@@ -3,11 +3,21 @@
 
     angular
         .module("app")
-        .controller("reportsCtrl", ["reportService", "boatService", "memberService", "boatStatusLogService",
-            function (reportService, boatService, memberService, boatStatusLogService) {
+        .controller("reportsCtrl", ["reportService", "boatService", "memberService", "boatStatusLogService", "boatStatusService",
+            function (reportService, boatService, memberService, boatStatusLogService, boatStatusService) {
                 var vm = this;
 
                 vm.loading = false;
+                vm.report = null;
+                vm.equipmentReport = null;
+                vm.damagedBoats = null;
+                vm.boatStatusLog = null;
+                vm.boatStatusLogSearchResults = null;
+                vm.boats = null;
+                vm.members = null;
+                vm.individualMileageReport = null;
+                vm.clubMileageReport = null;
+                vm.boatStatuses = null;
 
                 vm.loadEquipmentUsageReport = function (startDate, endDate) {
                     vm.loading = true;
@@ -30,10 +40,27 @@
                 vm.showEditBoatStatusLogModal = function(boatStatusLogId) {
                     boatStatusLogService.getBoatStatusLogById(boatStatusLogId).then(function(response) {
                         vm.boatStatusLog = response.data;
+                        setTimeout(function () { angular.element("#boatName").selectpicker("refresh"); }, 50);
+                        setTimeout(function () { angular.element("#status").selectpicker({ liveSearching: true }); }, 50);
+                        angular.element("#boat-status-log-modal").modal("show");
                     }, function() {
-                        console.log("Error in getBoatStatusLogById()")
+                        console.log("Error in getBoatStatusLogById()");
                     });
                 };
+
+                vm.addEditBoatStatusLog = function(boatStatusLog) {
+                    boatStatusLogService.editBoatStatusLog(boatStatusLog).then(function() {
+                        toastr.success("Successfully edited the boat status log.");
+                    }, function() {
+                        console.log("Error in editBoatStatusLog()");
+                    });
+                };
+
+                boatStatusService.getAllBoatStatusesAlphabetical().then(function(response) {
+                    vm.boatStatuses = response.data;
+                }, function() {
+                    console.log("Error in getAllBoatStatusesAlphabetical()");
+                });
 
                 vm.searchBoatStatusLog = function (searchParams) {
                     vm.loading = true;
